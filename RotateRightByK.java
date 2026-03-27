65f35bab594773bebabdc50ab40a2664ef430102
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-27 15:53:36 +0000

    rotate right by k

---
+++ RotateRightByK.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class RotateRightByK {
+    public static void reverse(int[] arr, int s, int e){
+        while(s<e){
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,6,7,8,9};
+        int k = 2;
+        int n = arr.length;
+         k = k%n;
+
+         reverse(arr, 0, n-1);
+         reverse(arr, 0, k-1);
+         reverse(arr, k, n-1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
