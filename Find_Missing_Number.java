99f00ba5b214cefcc14afa305d677ba527c15b8f
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-27 15:24:55 +0000

    find the missing number

---
+++ Find_Missing_Number.java
@@ -0,0 +1,16 @@
+public class Find_Missing_Number {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,6,7,8,9};
+        int n = arr.length+1;
+
+        int sum = n*(n+1)/2;
+        int actual = 0;
+
+        for(int i : arr){
+            actual +=i;
+        }
+       System.out.println(sum-actual);
+
+       
+    }
+}
