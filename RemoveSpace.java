a69128e1a5e9075e7d15d1f894d07849cd46bd3d
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-29 03:57:09 +0000

    remove space in a string or a word

---
+++ RemoveSpace.java
@@ -0,0 +1,8 @@
+public class RemoveSpace{
+    public static void main(String[] args) {
+        String s = "Di ves h";
+
+        String result = s.replace(" ","");
+        System.out.println(result);
+    }
+}
