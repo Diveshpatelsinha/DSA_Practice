8ee6d66032da69831eb42d2448107dba3f806a29
Divesh Patel <Diweshpatelsinha@gmail.com>
2026-03-30 13:30:35 +0530

    LongestSubstring

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,25 +1,36 @@
 import java.util.*;
-public class LongestSubstring{
-    public static void main(String[] args){
-        String s = "abcabcbb";
 
-        HashSet<Character> set = new HashSet<>();
-        int left = 0;
+class LongestSubstring {
+
+    public static void main(String[] args) {
+        String s = "Programming world";
         int maxlen = 0;
-
-        for(int right = 0; right<s.length();right++){
-
-
-            while(set.contains(s.charAt(right))){
+        int left  = 0;
+        String sub = "";
+        HashSet<Character> set = new HashSet<>();
+        int r = 0;
+        for( r = 0; r<s.length(); r++){
+            while(set.contains(s.charAt(r))){
                 set.remove(s.charAt(left));
                 left++;
             }
 
-
-
-            set.add(s.charAt(right));
-            maxlen = Math.max(maxlen, right-left+1);
+            String word = "";
+            for(int k = left;k<=r;k++){
+                word +=s.charAt(k);
+                System.out.println(word);
+            }
+            set.add(s.charAt(r));
+            
+            maxlen = Math.max(maxlen, r-left+1);
         }
-        System.out.println(maxlen);
+        // for(char c : set ){
+        //     System.out.print(c);
+        // }
+        for(int i = left;i<r;i++){
+            sub+=s.charAt(i);
+        }
+        System.out.println("longest substring is: "+sub);
+        System.out.println("length of longest substring is: "+maxlen);
     }
 }
